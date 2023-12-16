4a37b9886115e116aadcb7078c8bf07b4712d84b
semnil5202 <semnil5202@gmail.com>
2023-12-16 18:53:50 +0900

    feat: Profile 영역 구현

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import { Button } from '../common/Button';
+import { Typography } from '../common/Typography';
+import profileImage from '../../public/assets/profile.png';
+import styles from './Profile.module.css';
+
+function Profile() {
+  return (
+    <>
+      <section className={styles.profile}>
+        <div className={styles.imageContainer}>
+          <Image
+            src={profileImage}
+            alt="프로필 이미지"
+            width={400}
+            height={400}
+          />
+        </div>
+
+        <div>
+          <div>
+            <Typography tag="h2" size="large" weight="bold" color="default">
+              안녕하세요.
+            </Typography>
+            <Typography tag="h2" size="large" weight="bold" color="default">
+              000하는
+            </Typography>
+            <Typography tag="h2" size="large" weight="bold" color="default">
+              프론트엔드 개발자 이세민입니다.
+            </Typography>
+          </div>
+          <div className={styles.buttonContainer}>
+            <Button tag="a" href="https://github.com/semnil5202" target="blank">
+              Github
+            </Button>
+            <Button tag="a" href="https://velog.io/@semnil5202" target="blank">
+              Blog
+            </Button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Profile;
