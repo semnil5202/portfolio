bf8e2147e96f08a7830b137937c81f36d73e1446
semnil5202 <semnil5202@gmail.com>
2023-12-16 17:32:18 +0900

    feat: common Typography 다형성컴포넌트 구현

---
+++ src/components/common/Typography/Typography.tsx
@@ -0,0 +1,38 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react';
+import styles from './Typography.module.css';
+
+type SizeType = 'small' | 'default' | 'medium' | 'large';
+type WeightType = 'default' | 'bold';
+type ColorType = 'default' | 'primary';
+
+type Props<T extends ElementType> = {
+  tag?: T;
+  size?: SizeType;
+  weight?: WeightType;
+  color?: ColorType;
+  children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
+
+function Typography<T extends ElementType>({
+  tag,
+  size = 'default',
+  weight = 'default',
+  color = 'default',
+  children,
+  ...attributes
+}: Props<T>) {
+  const Tag = tag || 'span';
+
+  return (
+    <Tag
+      className={`${styles[`size-${size}`]} ${styles[`weight-${weight}`]} ${
+        styles[`color-${color}`]
+      }`}
+      {...attributes}
+    >
+      {children}
+    </Tag>
+  );
+}
+
+export default Typography;
