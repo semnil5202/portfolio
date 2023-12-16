2f471d5d211043b3256e16dddc1bd053ba33140b
semnil5202 <semnil5202@gmail.com>
2023-12-16 18:22:03 +0900

    feat: common Button 다형성 컴포넌트 구현

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,28 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react';
+import styles from './Button.module.css';
+
+type TagType = 'button' | 'a';
+type ButtonType = 'primary' | 'secondary';
+
+type Props<T extends ElementType> = {
+  tag?: T extends TagType ? T : never;
+  type?: ButtonType;
+  children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
+
+function Button<T extends ElementType>({
+  tag,
+  type = 'primary',
+  children,
+  ...attribute
+}: Props<T>) {
+  const Tag = tag || 'button';
+
+  return (
+    <Tag className={`${styles.style} ${styles[`type-${type}`]}`} {...attribute}>
+      {children}
+    </Tag>
+  );
+}
+
+export default Button;
