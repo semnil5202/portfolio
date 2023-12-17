a1a957796d5d75d80998f8f9cc5bc16938326c19
semnil5202 <semnil5202@gmail.com>
2023-12-17 20:08:07 +0900

    refactor: Strength description interface 타입 수정 (string -> string[])

--- src/components/Strength/Strength.tsx
+++ src/components/Strength/Strength.tsx
@@ -3,18 +3,22 @@ import styles from './Strength.module.css';
 
 interface Props {
   title: string;
-  description: string;
+  descriptions: string[];
 }
 
-export default function Strength({ title, description }: Props) {
+export default function Strength({ title, descriptions }: Props) {
   return (
     <div className={styles.wrapper}>
       <div className={styles.titleWrapper}>
         <Typography size="medium">{title}</Typography>
       </div>
-      <div className={styles.descriptionWrapper}>
-        <Typography tag="p">{description}</Typography>
-      </div>
+      <ul className={styles.descriptionWrapper}>
+        {descriptions.map((description, idx) => (
+          <Typography key={`${description}-${idx}`} tag="li">
+            {description}
+          </Typography>
+        ))}
+      </ul>
     </div>
   );
 }
