0ba66fac57cc9e03128957eb4d73a1722bf8bb41
semnil5202 <semnil5202@gmail.com>
2023-12-17 21:07:32 +0900

    feat: ProjectCard 컴포넌트 구현
    동적 라우팅 Link 까지 구현한다.

---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,39 @@
+import Image, { StaticImageData } from 'next/image';
+import styles from './ProjectCard.module.css';
+import { Typography } from '../common/Typography';
+import Link from 'next/link';
+import { Button } from '../common/Button';
+
+interface Props {
+  imageSrc: StaticImageData;
+  title: string;
+  description: string;
+  personnel: string;
+  slug: string;
+}
+
+export default function ProjectCard({
+  imageSrc,
+  title,
+  description,
+  personnel,
+  slug,
+}: Props) {
+  return (
+    <article className={styles.wrapper}>
+      <div className={styles.imageWrapper}>
+        <Image src={imageSrc} alt="프로젝트 이미지" width={340} height={340} />
+      </div>
+      <div className={styles.contentWrapper}>
+        <Typography size="medium">{title}</Typography>
+        <Typography>{description}</Typography>
+        <Typography color="gray">{personnel}</Typography>
+      </div>
+      <div className={styles.buttonWrapper}>
+        <Link href={`/${slug}`}>
+          <Button>자세히 보기</Button>
+        </Link>
+      </div>
+    </article>
+  );
+}
