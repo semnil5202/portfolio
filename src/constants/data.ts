24ceb97d861a88c488dacd07f33d51cd5a41af4c
semnil5202 <semnil5202@gmail.com>
2024-01-01 23:09:32 +0900

    docs: 오탈자 수정 및 구어체 수정

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -37,7 +37,7 @@ export const STRENGTHS = [
     title: 'React',
     description: [
       '• React를 사용하여 팀 프로젝트로 웹 서비스를 개발한 경험이 있습니다.',
-      '• React와 외부시스템인 지도를 결합해본 경험이 있습니다. ',
+      '• React와 외부시스템인 지도를 결합해본 경험이 있습니다.',
       '• Recoil, Zustand 등 상태 관리 라이브러리 사용 경험이 있습니다.',
       '• React Lazy를 활용한 성능 최적화를 진행한 경험이 있습니다.',
       '• ErrorBoundary, Suspense를 활용한 선언형 프로그래밍에 능숙합니다.',
@@ -66,7 +66,7 @@ export const STRENGTHS = [
       '• TypeScript를 사용하여 프로젝트를 진행한 경험이 있습니다.',
       '• Webpack을 활용한 번들러 사용 경험이 있습니다.',
       '• Jest, Cypress를 활용한 테스트 작성 및 환경 구축 경험이 있습니다.',
-      '• Storybook 작성 및 배포, Addon을 활용한 경험이 있습니다.',
+      '• Storybook 작성 및 배포, Addon을 활용할 수 있습니다.',
       '• MSW를 활용하여 Mock API를 만들 수 있습니다.',
     ],
   },
@@ -158,7 +158,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
       name: '괜찮을지도',
       intros: [
         '개인의 관심사를 다양한 지도로 만들고 공유할 수 있는 서비스입니다.',
-        '기존의 지도 서비스는 개인의 관심사를 기록하는데 용이하지만, 다른 사람과 정보를 공유하는데 불편하다는 점에서 이를 개선하고자 프로젝트를 시작하였습니다.',
+        '기존의 지도 서비스는 개인의 관심사를 기록하는데 용이하지만, 다른 사람과 정보를 공유하는데 불편하다는 점에서 이를 개선하고자 프로젝트를 시작했습니다.',
         '서로 다른 주제의 지도를 한 번에 모아보거나, 마음에 드는 정보만 골라 나만의 지도로 재탄생 시킬 수도 있습니다.',
       ],
       period: '2023.06 - 2023.12',
@@ -189,8 +189,8 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '수백 개 이상의 마커를 보유한 지도의 경우, 마커가 지도를 가려 위치 확인이 어렵고 지도를 조작할 때 버벅대는 불편함이 있었습니다.',
         ],
         solves: [
-          '지도의 줌 레벨에 따라 마커의 지름이 차지하는 실제 거리를 구하고, 이를 바탕으로 마커 간 겹침 여부를 판단하여 마커 클러스터링을 진행하였습니다.',
-          '스크린 사이즈에 해당하는 마커들만 동적으로 렌더링함으로써 렌더링 프레임을 48fps에서 71fps로 개선하였습니다. (75fps 모니터 기준)',
+          '지도의 줌 레벨에 따라 마커의 지름이 차지하는 실제 거리를 구하고, 이를 바탕으로 마커 간 겹침 여부를 판단하여 마커 클러스터링을 진행했습니다.',
+          '스크린 사이즈에 해당하는 마커들만 동적으로 렌더링함으로써 렌더링 프레임을 48fps에서 71fps로 개선했습니다. (75fps 모니터 기준)',
         ],
         result: {
           description: {
@@ -208,8 +208,8 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '지도 위 마커를 클릭했을 때 사이드바를 확장하여 세부 정보를 보여줄 수 있도록 해야 했고, 사이드 바의 리스트를 클릭하면 지도를 해당 정보의 위치로 이동 및 줌 인 시켜야 했습니다.',
         ],
         solves: [
-          '지도 이벤트 핸들링 로직에 디바운싱을 걸어 마지막 이벤트에만 로직을 수행함으로써 서버 통신 비용을 절감하고 효율적으로 지도를 조작할 수 있도록 하였습니다.',
-          'Route State 및 쿼리 파라미터를 사용하여 마커 및 사이드 바 리스트 클릭 여부를 확인할 수 있도록 함으로써 사이드 바와 지도 간 상호작용과 URL 공유를 가능케 했습니다.',
+          '지도 이벤트 핸들링 로직에 디바운싱을 걸어 마지막 이벤트에만 로직을 수행함으로써 서버 통신 비용을 절감하고 효율적으로 지도를 조작할 수 있도록 했습니다.',
+          'Route State 및 쿼리 파라미터를 사용하여 마커 및 사이드 바 리스트 클릭 여부를 확인할 수 있도록 함으로써 사이드 바와 지도 간 상호작용과 URL 공유를 가능하게 했습니다.',
         ],
       },
       {
@@ -220,8 +220,8 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '단순 콘텐츠 나열 형태로 사용자에게 우선적으로 노출하고자 한 콘텐츠가 잘 전달되지 못했습니다.',
         ],
         solves: [
-          '콘텐츠 소비자를 중심으로 UI를 재배치하기 위해 메인 페이지에서부터 존재하던 지도를 분리하고, 지도를 필요로 하는 페이지에서만 동적으로 보여줄 수 있도록 하였습니다.',
-          '프로젝트에서 공용으로 사용할 수 있는 Swiper를 구현함으로써, 사용자에게 노출하고자 하는 콘텐츠를 우선적으로 배치할 수 있었습니다. 또한 Swiper는 NPM에 배포하였습니다.',
+          '콘텐츠 소비자를 중심으로 UI를 재배치하기 위해 메인 페이지에서부터 존재하던 지도를 분리하고, 지도를 필요로 하는 페이지에서만 동적으로 보여줄 수 있도록 했습니다.',
+          '프로젝트에서 공용으로 사용할 수 있는 Swiper를 구현함으로써, 사용자에게 노출하고자 하는 콘텐츠를 우선적으로 배치할 수 있었습니다. 또한 Swiper는 NPM에 배포했습니다.',
         ],
         result: {
           description: {
@@ -237,8 +237,8 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '서비스 규모가 방대해짐에 따라 번들 사이즈가 커져 초기 로딩 시간이 길어지는 현상이 발생했습니다.',
         ],
         solves: [
-          'Webpack의 sideEffects 속성을 false로 지정함으로써 Tree Shaking을 진행하였습니다.',
-          'Page 컴포넌트 단위로 Dynamic Import 방식과 React.lazy, Suspense를 사용하여 Code Splitting을 진행하였습니다.',
+          'Webpack의 sideEffects 속성을 false로 지정함으로써 Tree Shaking을 진행했습니다.',
+          'Page 컴포넌트 단위로 Dynamic Import 방식과 React.lazy, Suspense를 사용하여 Code Splitting을 진행했습니다.',
         ],
         result: {
           description: {
@@ -252,8 +252,8 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '스크린 리더기를 통해 서비스를 이용해 보았을 때, 정상적으로 사용하기에는 어려움이 있었습니다.',
         ],
         solves: [
-          '시맨틱 태그, aria-label 등을 적용하여 스크린 리더기를 사용하는 사용자들에게 페이지에 대한 이해를 높이고 보다 편리하게 사용할 수 있도록 하였습니다.',
-          'Chrome, Safari 등 목표한 브라우저에서 서비스의 모든 플로우가 동작 가능하도록 웹 표준을 준수하였습니다.',
+          '시맨틱 태그, aria-label 등을 적용하여 스크린 리더기를 사용하는 사용자들에게 페이지에 대한 이해를 높이고 보다 편리하게 사용할 수 있도록 했습니다.',
+          'Chrome, Safari 등 목표한 브라우저에서 서비스의 모든 플로우가 동작 가능하도록 웹 표준을 준수했습니다.',
         ],
       },
       {
@@ -262,7 +262,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
           'PC 환경을 기준으로 서비스를 개발하였기에 모바일, 태블릿 환경에서 서비스를 쾌적하게 이용하기 어려운 점이 있었습니다.',
         ],
         solves: [
-          'CSS의 Media Query, Web API의 MatchMedia를 사용하여 PC, 태블릿, 모바일 환경에서 최적의 사용자 경험을 제공할 수 있도록 하였습니다.',
+          'CSS의 Media Query, Web API의 MatchMedia를 사용하여 PC, 태블릿, 모바일 환경에서 최적의 사용자 경험을 제공할 수 있도록 했습니다.',
         ],
         result: {
           description: {
@@ -278,8 +278,8 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '디자이너의 부재로 서비스 전반의 UI/UX를 3명의 프론트엔드 인원이 나눠서 개발해야 하는 상황이었습니다. 이 경우 일관되지 않은 UI/UX로 이어져 사용자 경험을 해칠 우려가 있었습니다.',
         ],
         solves: [
-          'Figma를 사용하여 프로젝트 전반의 와이어 프레임 및 프로토타입을 구현함으로써 일관된 UI/UX 구축하였습니다.',
-          '매 스프린트 별로 변경사항이 생길 경우 이에 대응하여 와이어 프레임 및 프로토타입을 수정하여 제공하였습니다.',
+          'Figma를 사용하여 프로젝트 전반의 와이어 프레임 및 프로토타입을 구현함으로써 일관된 UI/UX 구축했습니다.',
+          '매 스프린트 별로 변경사항이 생길 경우 이에 대응하여 와이어 프레임 및 프로토타입을 수정하여 제공했습니다.',
         ],
       },
     ],
@@ -312,7 +312,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '프론트엔드에서 API 로직을 작성하려면 백엔드 개발이 이루어진 후에 가능하기에, 개발 주기가 비효율적으로 흘러가는 불편함이 있었습니다.',
         ],
         solves: [
-          'MSW를 사용하여 Mock API를 만듦으로써 서버 의존성 없이 API 로직을 작성할 수 있도록 하였고, 추후 End Point만 교체하는 것으로 실제 서버와의 통신이 가능하도록 하였습니다.',
+          'MSW를 사용하여 Mock API를 만듦으로써 서버 의존성 없이 API 로직을 작성할 수 있도록 하였고, 추후 End Point만 교체하는 것으로 실제 서버와의 통신이 가능하도록 했습니다.',
         ],
       },
       {
@@ -331,7 +331,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '다른 분야 직군과 진행 상황을 공유할 때 서로의 전문성이 달라 의사소통을 하는데 어려움이 있었습니다.',
         ],
         solves: [
-          'Storybook을 통해 컴포넌트를 시각적으로 검토하고 테스트할 수 있는 환경을 제공함으로써 보다 원할한 의사소통을 가능하게 하였습니다.',
+          'Storybook을 통해 컴포넌트를 시각적으로 검토하고 테스트할 수 있는 환경을 제공함으로써 보다 원할한 의사소통을 가능하게 했습니다.',
         ],
       },
       {
@@ -340,7 +340,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '모바일 및 태블릿 환경에서 웹 페이지를 확인하기에는 불편함이 있었습니다.',
         ],
         solves: [
-          'CSS Media Query를 활용하여 PC, 태블릿, 모바일 환경에서 최적의 사용자 경험을 제공할 수 있도록 하였습니다.',
+          'CSS Media Query를 활용하여 PC, 태블릿, 모바일 환경에서 최적의 사용자 경험을 제공할 수 있도록 했습니다.',
         ],
         result: {
           imageSrc: shoppingCartResponsiveImage,
@@ -384,8 +384,8 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '모바일 환경은 가상 키보드를 사용하기에 각 폼에서 요구하는 정보에 맞게 숫자 키보드, 문자 키보드 등으로 변환할 필요성이 있었습니다.',
         ],
         solves: [
-          'React의 Ref와 제어 컴포넌트 방식을 활용하여 Auto Focus를 구현함으로써, 모바일 환경에서 입력 창을 클릭하는 과정 없이 보다 편리하게 입력할 수 있도록 하였습니다.',
-          'input 태그의 type 속성을 활용하여 각 입력 폼에 맞게 가상 키보드의 종류를 변경함으로써, 보다 편리하게 입력할 수 있도록 하였습니다.',
+          'React의 Ref와 제어 컴포넌트 방식을 활용하여 Auto Focus를 구현함으로써, 모바일 환경에서 입력 창을 클릭하는 과정 없이 보다 편리하게 입력할 수 있도록 했습니다.',
+          'input 태그의 type 속성을 활용하여 각 입력 폼에 맞게 가상 키보드의 종류를 변경함으로써, 보다 편리하게 입력할 수 있도록 했습니다.',
         ],
       },
       {
@@ -394,7 +394,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '페어 프로그래밍과 코드 리뷰 과정에서 컴포넌트 구조 및 구현 이유 등을 설명할 때 명확한 의사 전달 도구가 필요했습니다.',
         ],
         solves: [
-          'Storybook을 사용한 CDD를 통해 컴포넌트를 시각적으로 문서화하고 조작해볼 수 있는 환경을 제공함으로써 보다 명확한 의사소통을 가능하게 하였습니다.',
+          'Storybook을 사용한 CDD를 통해 컴포넌트를 시각적으로 문서화하고 조작해볼 수 있는 환경을 제공함으로써 보다 명확한 의사소통을 가능하게 했습니다.',
         ],
       },
       {
@@ -403,7 +403,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
           'Modal 컴포넌트가 특정 컴포넌트와 의존성이 높아 재사용이 어려웠습니다.',
         ],
         solves: [
-          'Modal 컴포넌트의 Interface를 조정하고 React의 Portal를 활용하여 타 컴포넌트와의 의존성을 제거함으로써, 미션 전반에서 공용적으로 사용할 수 있도록 하였습니다.',
+          'Modal 컴포넌트의 Interface를 조정하고 React의 Portal를 활용하여 타 컴포넌트와의 의존성을 제거함으로써, 미션 전반에서 공용적으로 사용할 수 있도록 했습니다.',
         ],
         result: {
           description: {
@@ -440,7 +440,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
           "영화 인기 목록을 추가 조회할 때 '더 불러오기' 등과 같은 버튼을 반복적으로 클릭해야하는 불편함이 있었습니다.",
         ],
         solves: [
-          'IntersectionObserver를 활용하여 사용자가 마지막 영화 목록까지 스크롤 하였을 때 자동으로 추가 조회를 수행할 수 있도록 하였습니다.',
+          'IntersectionObserver를 활용하여 사용자가 마지막 영화 목록까지 스크롤 하였을 때 자동으로 추가 조회를 수행할 수 있도록 했습니다.',
         ],
       },
       {
@@ -450,7 +450,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '영화 포스터 이미지를 로딩하는 과정에서 Layout Shift가 발생하여 잘못된 조작을 하게 되는 불편함이 있었습니다.',
         ],
         solves: [
-          'Skeleton UI를 도입하여 영화 포스터 이미지가 로딩 중임을 사용자가 보다 명확하게 인지할 수 있도록 하고, Layout Shift를 개선하여 CLS 0을 달성하였습니다.',
+          'Skeleton UI를 도입하여 영화 포스터 이미지가 로딩 중임을 사용자가 보다 명확하게 인지할 수 있도록 하고, Layout Shift를 개선하여 CLS 0을 달성했습니다.',
         ],
         result: {
           imageSrc: movieListSkeletonImage,
@@ -462,7 +462,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '모바일 및 태블릿 환경에서 웹 페이지를 확인하기에는 불편함이 있었습니다.',
         ],
         solves: [
-          'CSS Media Query를 활용하여 PC, 태블릿, 모바일 환경에서 최적의 사용자 경험을 제공할 수 있도록 하였습니다.',
+          'CSS Media Query를 활용하여 PC, 태블릿, 모바일 환경에서 최적의 사용자 경험을 제공할 수 있도록 했습니다.',
         ],
         result: {
           imageSrc: movieListResponsiveImage,
@@ -528,7 +528,7 @@ export const PROJECT_DETAILS: ProjectDetails = {
           '서버 컴포넌트이기에 React 문법으로만 다형성 컴포넌트를 구현해야 했고, 사용처에서 컴포넌트 Tag와 무관한 Attribute를 사용할 우려가 있었습니다.',
         ],
         solves: [
-          'TypeScript를 활용하여 제네릭과 React의 ElementType, ComponentPropsWithoutRef 타입을 활용하여 다형성 컴포넌트를 구현함으로써, Tag에 따른 Attribute를 동적으로 지정할 수 있도록 하였습니다.',
+          'TypeScript를 활용하여 제네릭과 React의 ElementType, ComponentPropsWithoutRef 타입을 활용하여 다형성 컴포넌트를 구현함으로써, Tag에 따른 Attribute를 동적으로 지정할 수 있도록 했습니다.',
         ],
       },
       {
