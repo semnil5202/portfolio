a6eaaffcd77220496a8bac541635aad080ce7ce6
semnil5202 <semnil5202@gmail.com>
2024-04-02 04:06:36 +0900

    refactor: last updated 갱신

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import styles from './Footer.module.css';
 
 export default function Footer() {
-  return <footer className={styles.footer}>Last updated (2024. 03)</footer>;
+  return <footer className={styles.footer}>Last updated (2024. 04)</footer>;
 }
