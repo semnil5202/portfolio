d039e6f370f6ce1fc46df6e0b2617dec6eb7aafc
semnil5202 <semnil5202@gmail.com>
2024-05-11 03:23:33 +0900

    refactor: Navigation Swiper mouse Enter 영역 조정

--- src/app/[slug]/containers/Templates/components/NavigationSwiper.tsx
+++ src/app/[slug]/containers/Templates/components/NavigationSwiper.tsx
@@ -18,7 +18,7 @@ const NavigationSwiper = ({ setThumbsSwiper, navigations }: Props) => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-[3%] h-full bg-transparent z-50`}
+      className={`fixed top-0 left-0 w-[2%] h-full bg-transparent z-50`}
       onMouseEnter={() => setIsMouseEnter(true)}
       onMouseLeave={() => setIsMouseEnter(false)}
     >
