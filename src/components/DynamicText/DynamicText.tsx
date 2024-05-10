04ffd9b079f15ed177bb62f01a5c05a8f3b1e129
semnil5202 <semnil5202@gmail.com>
2024-05-11 04:21:58 +0900

    feat: 다이나믹 텍스트 반응형 적용

--- src/components/DynamicText/DynamicText.tsx
+++ src/components/DynamicText/DynamicText.tsx
@@ -74,13 +74,13 @@ export default function DynamicText({ texts }: Props) {
 
   return (
     <div className="flex w-max">
-      <h2 className="text-5xl leading-snug text-primary-dark font-bold">
+      <h2 className="text-5xl leading-snug text-primary-dark font-bold md:text-3xl md:leading-normal">
         {currentText}
       </h2>
       <span
         className={`text-5xl leading-snug text-primary-dark ${
           !isTyping && 'animate-flickering'
-        }`}
+        } md:text-3xl md:leading-normal`}
       >
         |
       </span>
