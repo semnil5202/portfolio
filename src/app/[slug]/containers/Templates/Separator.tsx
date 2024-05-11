7d4f156a271e1569611d9efac9e410624d2a795d
semnil5202 <semnil5202@gmail.com>
2024-05-12 08:22:59 +0900

    feat: 프로젝트 분리 페이지 md 반응형 브라우저 뷰포트 대응

--- src/app/[slug]/containers/Templates/Separator.tsx
+++ src/app/[slug]/containers/Templates/Separator.tsx
@@ -17,8 +17,10 @@ const Separator = ({ type, heading, link, svg }: Props) => {
     return (
       <section className="relative w-full h-full flex justify-center items-center">
         <div className="flex flex-col items-center">
-          <p className="text-7xl text-fff font-bold lg:text-6xl">{heading}</p>
-          <div className="w-[30%] h-[2px] bg-fff mt-10 mr-4 lg:w-[25%]" />
+          <p className="text-7xl text-fff font-bold lg:text-6xl md:text-[5vw]">
+            {heading}
+          </p>
+          <div className="w-[30%] h-[2px] bg-fff mt-10 lg:w-[25%] md:w-[20%] md:mt-[2vw]" />
         </div>
       </section>
     );
@@ -31,15 +33,18 @@ const Separator = ({ type, heading, link, svg }: Props) => {
         <div className="flex items-center">
           {Svg && (
             <div className="mr-4">
-              <Svg />
+              <Svg className="md:w-[4.8vw] md:h-[4.8vw]" viewBox="0 0 72 72" />
             </div>
           )}
-          <p className="text-7xl text-fff font-bold mr-4 lg:text-6xl">
+          <p className="text-7xl text-fff font-bold mr-4 lg:text-6xl md:text-[5vw]">
             {heading}
           </p>
-          <SVGNextIcon />
+          <SVGNextIcon
+            className="md:w-[4.8vw] md:h-[4.8vw]"
+            viewBox="0 0 72 72"
+          />
         </div>
-        <div className="w-[30%] h-[2px] bg-fff mt-10 mr-4 lg:w-[25%]" />
+        <div className="w-[30%] h-[2px] bg-fff mt-10 mr-4 lg:w-[25%] md:w-[15%] md:mt-[2vw]" />
       </Link>
     </section>
   );
