807f878bd20c191ade502e3d5f587067fbc25d60
semnil5202 <semnil5202@gmail.com>
2024-05-12 08:23:34 +0900

    refactor: 프로젝트 소개 페이지 md 반응형 tech 위치 보완

--- src/app/[slug]/containers/Templates/components/LinkIcon.tsx
+++ src/app/[slug]/containers/Templates/components/LinkIcon.tsx
@@ -13,7 +13,7 @@ const LinkIcon = ({ name, href, svg: Svg }: Props) => {
       target="_blank"
       className="flex items-center mb-5 md:mb-[0.1vw]"
     >
-      <div className="w-[40px] h-[40px] rounded-[50%] flex items-center justify-center mr-4 md:mr-[0.6vw]">
+      <div className="w-[40px] h-[40px] rounded-[50%] flex items-center justify-center mr-4 md:mr-0">
         <Svg className="md:w-[24px] md:h-[24px]" viewBox="0 0 40 40" />
       </div>
       <span className="text-fff font-extralight md:text-[1.6vw]">{name}</span>
