2c2ab953227257ab1efe22e803793aba4c18a84e
semnil5202 <semnil5202@gmail.com>
2024-05-12 09:28:21 +0900

    refactor: 모바일 환경에서 Swiper Navigation 터치 슬라이드로 열고 닫기 기능으로 보완

--- src/app/[slug]/containers/Templates/components/NavigationSwiper.tsx
+++ src/app/[slug]/containers/Templates/components/NavigationSwiper.tsx
@@ -2,7 +2,7 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel, Thumbs } from 'swiper/modules';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, TouchEventHandler, useState } from 'react';
 import Image, { StaticImageData } from 'next/image';
 
 interface Props {
@@ -12,17 +12,34 @@ interface Props {
 
 const NavigationSwiper = ({ setThumbsSwiper, navigations }: Props) => {
   const [isMouseEnter, setIsMouseEnter] = useState<boolean>(false);
+  const [prevTouch, setPrevTouch] = useState<React.Touch | null>(null);
   const thumbTranslate = isMouseEnter
     ? 'translate-x-[0%]'
     : 'translate-x-[-105%]';
+
+  const onTouchNavigation: TouchEventHandler = (event) => {
+    const touch = event.touches[0]!;
+
+    setPrevTouch(touch);
+    if (!prevTouch) return;
+
+    const diff = touch.pageX - prevTouch.pageX;
+
+    if (diff > 3) setIsMouseEnter(true);
+    if (diff < -3) setIsMouseEnter(false);
+  };
+
+  const onTouchEndNavigation: TouchEventHandler = () => {
+    setPrevTouch(null);
+  };
 
   return (
     <div
       className={`fixed top-0 left-0 w-[2%] h-full bg-transparent z-50 md:w-[5%]`}
       onMouseEnter={() => setIsMouseEnter(true)}
       onMouseLeave={() => setIsMouseEnter(false)}
-      onTouchStart={() => setIsMouseEnter(true)}
-      onTouchCancel={() => setIsMouseEnter(false)}
+      onTouchMove={onTouchNavigation}
+      onTouchEnd={onTouchEndNavigation}
     >
       <Swiper
         onSwiper={setThumbsSwiper}
@@ -39,6 +56,8 @@ const NavigationSwiper = ({ setThumbsSwiper, navigations }: Props) => {
           <SwiperSlide
             key={idx}
             className="w-full h-full relative cursor-pointer"
+            onTouchMove={onTouchNavigation}
+            onTouchEnd={onTouchEndNavigation}
           >
             <Image
               src={image}
