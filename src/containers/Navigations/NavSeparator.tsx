4c6101aded576967780543a94e0f8d6fc0ff9498
semnil5202 <semnil5202@gmail.com>
2024-06-13 02:34:54 +0900

    refactor: Separator 및 NavTemplateLayout 조정

--- src/containers/Navigations/NavSeparator.tsx
+++ src/containers/Navigations/NavSeparator.tsx
@@ -15,8 +15,8 @@ const NavSeparator = ({ type, heading, link, svg }: Props) => {
     return (
       <div className="relative w-full h-full flex justify-center items-center">
         <div className="flex flex-col items-center">
-          <p className="text-[10px] text-white font-bold">{heading}</p>
-          <div className="w-[30%] h-[0.5px] bg-white mt-[6px]" />
+          <p className="text-[8px] text-white font-bold">{heading}</p>
+          <div className="w-[30%] h-[0.5px] bg-white mt-[4px]" />
         </div>
       </div>
     );
@@ -28,13 +28,13 @@ const NavSeparator = ({ type, heading, link, svg }: Props) => {
         <div className="flex items-center">
           {Svg && (
             <div className="mr-1">
-              <Svg className="w-[12px] h-[12px]" viewBox="0 0 72 72" />
+              <Svg className="w-[10px] h-[10px]" viewBox="0 0 72 72" />
             </div>
           )}
-          <p className="text-[10px] text-white font-bold mr-1">{heading}</p>
-          <SVGNextIcon className="w-[12px] h-[12px]" viewBox="0 0 72 72" />
+          <p className="text-[8px] text-white font-bold mr-1">{heading}</p>
+          <SVGNextIcon className="w-[10px] h-[10px]" viewBox="0 0 72 72" />
         </div>
-        <div className="w-[25%] h-[0.5px] bg-white mt-[6px] mr-4" />
+        <div className="w-[25%] h-[0.5px] bg-white mt-[4px] mr-4" />
       </Link>
     </div>
   );
--- src/containers/Navigations/components/NavTemplateLayout.tsx
+++ src/containers/Navigations/components/NavTemplateLayout.tsx
@@ -11,7 +11,7 @@ const NavTemplateLayout = ({ heading, subHeading, children }: Props) => {
     <>
       <section className="w-full h-full relative px-[10%] py-[12%] flex flex-col items-center">
         <div className="w-full max-w-[1140px] h-[20%] flex flex-col justify-end">
-          <h2 className="text-[6px] text-white font-bold">{heading}</h2>
+          <h2 className="text-[5px] text-white font-bold">{heading}</h2>
           <div className="w-[12px] h-[0.5px] bg-white my-[2px]" />
           {subHeading ? (
             <h4 className="text-lg text-white font-extralight">{subHeading}</h4>
